48151f7f3ee50fb6127da29f279c244b5901ed3b
Francesco Mazzoli <francesco.mazzoli@erudify.com>
2014-02-13 03:26:48 +0100

    Use POST when appropriate

--- deploy-lock.hs
+++ deploy-lock.hs
@@ -5,7 +5,7 @@ import           Prelude                               hiding (catch)
 import           Control.Applicative                   ((<|>))
 import           Control.Concurrent.MVar               (MVar, newMVar, modifyMVar, readMVar)
 import           Control.Exception                     (throwIO, catch)
-import           Control.Monad                         (forM, mzero, when, forM_, join, void)
+import           Control.Monad                         (forM, mzero, when, forM_, join)
 import           Control.Monad.IO.Class                (liftIO)
 import           Control.Monad.Trans.Maybe             (MaybeT(MaybeT), runMaybeT)
 import           Crypto.Random                         (genBytes, newGenIO)
@@ -105,74 +105,69 @@ lockLockForm :: LockName -> Lock -> H.Html
 lockLockForm name lock =
     case lockStatus lock of
       Available ->
-        H.form ! A.action (fromString ("/lock/" ++ name)) $ do
+        H.form ! A.action (fromString ("/lock/" ++ name)) ! A.method "POST" $ do
           H.input ! A.type_ "text" ! A.name "user"
           H.input ! A.type_ "submit" ! A.value "Lock"
       Locked _ _ ->
         return ()
 
-releaseLockLink :: LockName -> Lock -> Maybe B.ByteString -> H.Html
-releaseLockLink name lock mbSecret =
+releaseLockForm :: LockName -> Lock -> Maybe B.ByteString -> H.Html
+releaseLockForm name lock mbSecret =
     case lockStatus lock of
       Available  -> return ()
       Locked _ _ ->
-        H.a ! A.href (fromString ("/release/" ++ name ++ query))
-            $ H.toHtml text
+        H.form ! A.action (fromString ("/release/" ++ name ++ query)) ! A.method "POST" $ do
+            H.input ! A.type_ "submit" ! A.value text
   where
     query = case mbSecret of
       Nothing      -> "?force=1"
       Just _secret -> ""
-    text :: String = case mbSecret of
+    text = case mbSecret of
       Nothing      -> "Force release"
       Just _secret -> "Release"
 
-renderBody :: String -> H.Html -> H.Html
-renderBody title body = H.html $ do
-     H.head $ do
-         H.title $ H.toMarkup title
-         -- link ! rel "stylesheet" ! type_ "text/css" ! href "screen.css"
-     H.body $ body
-
-renderLockBody :: RenderEnv -> String -> H.Html -> H.Html
-renderLockBody env title body = renderBody title $ do
-    H.ul $ do
-      forM_ env $ \(name, lock, mbSecret) ->
-        H.li $ do
-          H.b (fromString name)
-          " - " >> case lockStatus lock of
-             Available     -> "Available"
-             Locked _ user -> H.toHtml $ "Locked by " ++ user
-          void $ if lockAvailable lock then "" else " - "
-          releaseLockLink name lock mbSecret
-          lockLockForm name lock
-    H.hr
-    body
+renderBody :: RenderEnv -> String -> H.Html -> H.Html
+renderBody env title body = do
+    H.head $ do
+      H.title $ H.toMarkup title
+    H.body $ do
+      body
+      H.hr
+      H.ul $ do
+        forM_ env $ \(name, lock, mbSecret) ->
+          H.li $ do
+            H.b (fromString name)
+            " - " >> case lockStatus lock of
+               Available     -> "Available"
+               Locked _ user -> H.toHtml $ "Locked by " ++ user
+            releaseLockForm name lock mbSecret
+            lockLockForm name lock
 
 renderAlreadyLocked :: LockName -> String -> RenderEnv -> H.Html
 renderAlreadyLocked name user env =
-    renderLockBody env (name ++ " is already locked!") $ do
+    renderBody env (name ++ " is already locked!") $ do
       H.toHtml $ name ++ " is already locked by " ++ user ++ "."
 
 renderLockedSuccesfully :: LockName -> B.ByteString -> RenderEnv -> H.Html
 renderLockedSuccesfully name secret env =
-    renderLockBody env (name ++ " was locked successfully!") $ do
+    renderBody env (name ++ " was locked successfully!") $ do
       H.toHtml $ name ++ " was locked successfully!"
       H.br
       H.toHtml $ "Your secret is " ++ show secret
 
 renderAlreadyReleased :: LockName -> RenderEnv -> H.Html
 renderAlreadyReleased name env =
-    renderLockBody env (name <> " is available!") $ do
+    renderBody env (name <> " is available!") $ do
       H.toHtml $ "I can't release " ++ name ++ ", because it's already released."
 
 renderReleased :: LockName -> RenderEnv -> H.Html
 renderReleased name env =
-    renderLockBody env (name ++ " has been released.") $ do
+    renderBody env (name ++ " has been released.") $ do
       H.toHtml $ name ++ " has now been released."
 
 renderWrongCredentials :: RenderEnv -> H.Html
 renderWrongCredentials env =
-    renderLockBody env "Wrong credentials" "Wrong credentials"
+    renderBody env "Wrong credentials" "Wrong credentials"
 
 ------------------------------------------------------------------------
 -- Cookie
@@ -217,7 +212,7 @@ runRender locks r = do
     Snap.blaze $ r env
 
 lockLock :: Action (LockName -> Snap ())
-lockLock dataDir locksMV name = do
+lockLock dataDir locksMV name = Snap.method Snap.POST $ do
     user <- BC8.unpack <$> getParam "user"
     join $ liftIO $ modifyMVar locksMV $ \locks ->
       case HMS.lookup name locks of
@@ -240,7 +235,7 @@ lockLock dataDir locksMV name = do
                      )
 
 releaseLock :: Action (LockName -> Snap ())
-releaseLock dataDir locksMV name = do
+releaseLock dataDir locksMV name = Snap.method Snap.POST $ do
     force <- fromMaybe False . fmap (const True) <$> Snap.getParam "force"
     mbSecret <- readSecretCookie name
     join $ liftIO $ modifyMVar locksMV $ \locks -> do
@@ -271,7 +266,7 @@ releaseLock dataDir locksMV name = do
 listLocks :: MVar Locks -> Snap ()
 listLocks locksMV = do
     locks <- liftIO $ readMVar locksMV
-    runRender locks $ \env -> renderLockBody env "Index" $ return ()
+    runRender locks $ \env -> renderBody env "Index" $ return ()
 
 run :: Action (Snap ())
 run dataDir locksMV =
